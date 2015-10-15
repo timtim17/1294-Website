758a1be2abbb972d1c375250cd0a098a9db0d308
Austin Jenchi <timtim17@users.noreply.github.com>
2015-10-15 10:05:49 -0700

    Start WIP programming resources page
    Just a page for things that programmers need access to, like a link to documentation or a signup form.

---
+++ about/forms/resources/programming/index.php
@@ -0,0 +1,50 @@
+<?php 
+    require($_SERVER['DOCUMENT_ROOT'].'/includes/upper_header.php'); 
+?>
+    <title>1294 | Programming Resources</title>
+    <meta name="description" content="All the resorces you'll need for the software team."/>
+<?php
+    require($_SERVER['DOCUMENT_ROOT'].'/includes/lower_header.php');
+?>
+<div class="body-container top-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="center-block text-center">
+                    <h1 class="center-block">Programming Resources</h1>
+                    <p class="lead">All the things you would ever need (for programming)</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <h3>WPILib</h3>
+                <hr class="hr-underline">
+                <p class=""></p> 
+                <div class="row">
+                    <div class="col-xs-12">
+                        <ul class="no-bullets">
+                            <li><i class="fa fa-book icon-bullet"></i><a href="http://wpilib.screenstepslive.com/s/4485">WPILib Screensteps</a></li>
+                            <li><i class="fa fa-book icon-bullet"></i><a href="http://first.wpi.edu/FRC/roborio/release/docs/java/">WPILib Javadoc</a></li>
+                        </ul>
+                        <!--br>
+                        <ul class="no-bullets">
+                            <li><i class="fa fa-laptop icon-bullet"></i><a href="#">Installing Eclipse</a></li>
+                            <li><i class="fa fa-file-code-o icon-bullet"></i><a href="#">Accessing Sample Programs</a></li>
+                        </-->
+                        <br>
+                        <ul class="no-bullets">
+                            <li><i class="fa fa-file-text-o icon-bullet"></i><a href="#">Mailing List Signup</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+    
+    $ExtraFooterTags = '';
+    require($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
+ ?>016
