09dbde182eee65ed8099bed882b3934d3812cca2
Austin Jenchi <timtim17@users.noreply.github.com>
2015-11-23 11:36:46 -0800

    Update live_stream.php

--- includes/live_stream.php
+++ includes/live_stream.php
@@ -11,7 +11,7 @@
         <div class="row">
             <!--<div class="col-lg-10 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 col-lg-offset-1">-->
             <div class="col-xs-12">
-                <div class="embed-responsive embed-responsive-16by9" id="">
+                <div class="embed-responsive embed-responsive-16by9">
                     <iframe id="Live-Stream" class="embed-responsive-item" src="http://www.ustream.tv/embed/20054416?v=3&amp;wmode=direct&wmode=opaque" scrolling="no" frameborder="0" style="border: 0px none transparent;" seamless></iframe>
                 </div>
             </div>
@@ -23,7 +23,7 @@
                         <tr style="width:100%;">
                             <?php
                             /*
-                            *The following code is to get ride of the calendar button when the event is in progress or when the event has passed
+                            *The following code is to get rid of the calendar button when the event is in progress or when the event has passed
                             *We'll eventually be able to do this by checking the event database
                             */
                             $eventDate='26-03-2014';
@@ -58,7 +58,7 @@
                             <td>
                                 <a href="http://www.ustream.tv/channel/pnwfirst-blue" data-placement="top" data-toggle="tooltip" title="Go to ustream.com to view this live stream directly" class="add-tooltip btn btn-default">
                                     <span class="glyphicon glyphicon-facetime-video"></span> Direct Video Stream
-                                    <!--Well eventually get the link through the database and do everything automatically-->
+                                    <!--We'll eventually get the link through the database and do everything automatically-->
                                 </a>
                             </td>
                             <td>
@@ -68,7 +68,7 @@
                             </td>
                             <?php
                             } 
-                            /*End of PHP for date checking, well evetually do everything automatically with the database at some point*/
+                            /*End of PHP for date checking, we'll evetually do everything automatically with the database at some point*/
                             ?>
                         </tr>
                     </tbody>
